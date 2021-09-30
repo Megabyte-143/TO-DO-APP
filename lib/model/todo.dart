ab88f323a3b832b9b0d6ec650dd27734bef15368
Yash Garg <yashygkf@gmail.com>
2021-09-30 15:59:41 +0530

    setup todo model

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -1,33 +1,20 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:todo_app/widgets/utils.dart';
-
-class TodoField {
-  static const createdTime = "createdTime";
-}
-
 class Todo {
   Todo({
     required this.createdTime,
     required this.title,
     this.isComplete = false,
-    required this.id,
+    required this.todoID,
   });
+
+  ///
   DateTime createdTime;
+
+  ///
   String title;
+
+  ///
   bool isComplete;
-  String id;
 
-  static Todo fromJson(Map<String, dynamic> json) => Todo(
-        createdTime: Utils.toDateTime(json["createdTime"] as Timestamp ),
-        title: json["title"].toString(),
-        id: json["id"].toString(),
-        isComplete: json["isComplete"] as bool,
-      );
-
-  Map<String, dynamic> toJson() => {
-        "createdTime": Utils.fromDateTimeToJson(createdTime),
-        "title": title,
-        "id": id,
-        "isComplete": isComplete
-      };
+  ///
+  String todoID;
 }

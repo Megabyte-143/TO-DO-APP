43db80fe5d7ea185ac94293a4c8093d5622b8664
Yash Garg <yashygkf@gmail.com>
2021-09-30 16:01:15 +0530

    fetching the data from firebase

--- lib/widgets/main_view.dart
+++ lib/widgets/main_view.dart
@@ -1,16 +1,10 @@
-import 'dart:convert';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
+import "package:cloud_firestore/cloud_firestore.dart";
 import "package:flutter/material.dart";
-import "package:provider/provider.dart";
-import 'package:todo_app/widgets/task_widget.dart';
 
-import "../model/todo.dart";
-
-import "../provider/todo_provider.dart";
+import "task_widget.dart";
 
 class MainView extends StatefulWidget {
-  MainView({
+  const MainView({
     Key? key,
     required this.height,
     required this.width,
@@ -26,39 +20,53 @@ class MainView extends StatefulWidget {
 class _MainViewState extends State<MainView> {
   @override
   Widget build(BuildContext context) {
-    CollectionReference _todos = FirebaseFirestore.instance.collection("todo");
+   
+    
 
-    return FutureBuilder<Object>(
-        future: _todos.doc().get(),
-        builder: (context, AsyncSnapshot<Object> snapshot) {
-          final List<Todo> todos =
-              json.decode(json.encode(snapshot.data)) as List<Todo>;
-          final TodosProvider provider = Provider.of<TodosProvider>(context);
-          provider.setTodos(todos);
-          return Container(
-            height: widget.height * 0.7,
-            padding: EdgeInsets.only(
-              left: widget.width * 0.03,
-              right: widget.width * 0.03,
-              bottom: widget.height * 0.03,
-            ),
-            child: todos.isEmpty
-                ? const Center(
-                    child: Text("No Todos"),
-                  )
-                : ListView.builder(
-                    shrinkWrap: true,
-                    itemBuilder: (BuildContext ctx, int i) {
-                      return TaskWidget(
-                        height: widget.height,
-                        width: widget.width,
-                        index: i,
-                        todos: todos,
-                      );
-                    },
-                    itemCount: todos.length,
-                  ),
-          );
-        });
+    return Container(
+      height: widget.height * 0.7,
+      padding: EdgeInsets.only(
+        left: widget.width * 0.03,
+        right: widget.width * 0.03,
+        bottom: widget.height * 0.03,
+      ),
+      child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: FirebaseFirestore.instance.collection("todos").snapshots(),
+        builder: (
+          BuildContext context,
+          snapshot,
+        ) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasData) {
+            final List<QueryDocumentSnapshot<Map<String, dynamic>>> list =
+                snapshot.data!.docs;
+            return ListView.builder(
+              shrinkWrap: true,
+              itemBuilder: (
+                BuildContext ctx,
+                int i,
+              ) {
+                return TaskWidget(
+                  height: widget.height,
+                  width: widget.width,
+                  index: i,
+                  todoID: list[i]["todoID"].toString(),
+                  todoStatus: list[i]["todoStatus"] as bool,
+                  todoTitle: list[i]["todoTitle"].toString(),
+                );
+              },
+              itemCount: list.length,
+            );
+          } else {
+            return const Center(
+              child: Text("No to dos"),
+            );
+          }
+        },
+      ),
+    );
   }
 }

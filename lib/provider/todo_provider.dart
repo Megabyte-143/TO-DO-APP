93f687a876e5a99376aac0ea478f36be045e8c31
Yash Garg <yashygkf@gmail.com>
2021-09-30 16:00:40 +0530

    completed the todo provider

--- lib/provider/todo_provider.dart
+++ lib/provider/todo_provider.dart
@@ -1,29 +1,30 @@
 import "package:flutter/material.dart";
-import 'package:todo_app/api/Firebase_api.dart';
+import "../api/Firebase_api.dart";
 import "../model/todo.dart";
 
 class TodosProvider extends ChangeNotifier {
   List<Todo> _todos = <Todo>[];
-  List<Todo> get todos => _todos.toList();
+  List<Todo> get todos => _todos;
 
-  void addTodo(Todo todo) => FirebaseApi.createTodo(todo);
+  void addTodo(Todo todo) => FirebaseApi.addTodo(todo);
 
-  void setTodos(List<Todo> todos) =>
-      WidgetsBinding.instance!.addPostFrameCallback((Duration timeStamp) {
-        _todos = todos;
-        notifyListeners();
-      });
+  // void setTodos(List<Todo> todos) =>
+  //     WidgetsBinding.instance!.addPostFrameCallback((Duration timeStamp) {
+  //       _todos = todos;
+  //       notifyListeners();
+  //     });
 
-  void deleteTodo(Todo todo) => FirebaseApi.deleteTodo(todo);
+  void deleteTodo(String todoID) => FirebaseApi.deleteTodo(todoID);
 
-  bool todoStatus(Todo todo) {
-    todo.isComplete = !todo.isComplete;
-    FirebaseApi.updateTodo(todo);
-    return todo.isComplete;
+  void todoStatus(String todoID, bool todoStatus) {
+    // todo.isComplete = !todo.isComplete;
+    FirebaseApi.updateTodoStatus(todoStatus, todoID);
+    notifyListeners();
+    //return todo.isComplete;
   }
 
-  void updateTodo(String newValue, Todo todo) {
-    todo.title = newValue;
-    FirebaseApi.updateTodo(todo);
+  void updateTodo(String newValue, String todoID) {
+    // todo.title = newValue;
+    FirebaseApi.updateTodoText(newValue, todoID);
   }
 }

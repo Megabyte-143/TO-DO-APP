086d70c6252ab4eedc3b56cd0270782430725066
Yash Garg <yashygkf@gmail.com>
2021-10-01 23:45:57 +0530

    lint corrections

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import "package:firebase_core/firebase_core.dart";
 import "package:flutter/material.dart";
 import "package:provider/provider.dart";
 
-import "api/Firebase_api.dart";
-
 import "home_page_screen.dart";
-
-import "model/todo.dart";
 
 import "provider/todo_provider.dart";
 
@@ -21,13 +16,18 @@ Future main() async {
 /// Root Widget of the App
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Todo App",
-    theme: ThemeData(
-      backgroundColor: Colors.grey.shade400,
-      primarySwatch: Colors.grey,
-    ),
-    home: HomePageScreen(),
-  );
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => TodosProvider())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Todo App",
+        theme: ThemeData(
+          backgroundColor: Colors.grey.shade400,
+          primarySwatch: Colors.grey,
+        ),
+        home: HomePageScreen(),
+      ),
+    );
+  }
 }
